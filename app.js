865f210dea68021463306891c602b320c968f583
Jake <jake@Cynthias-MacBook-Pro.local>
2020-05-05 02:14:18 -0500

    revisions

--- app.js
+++ app.js
@@ -58,12 +58,84 @@ based on chosen skill (Var AttackModifier = hero.Attack - Enemy.AC
         Modal.style.display = "block";
     }
 
-    // start.onclick = function () {
-    //     Modal.style.display = "none";
-    // }
-    // start.onclick = function(event) {
-    //     if(event.target == start) {
-    //         Modal.style.display = "none";
-    //     }
-    // }
+    
 
+
+    
+function reflectMod() {
+    var StrScore = document.getElementById('StrScore').value;
+    document.getElementById("StrMod").value= Math.floor((StrScore - 10)/2);
+
+    var ConScore = document.getElementById('ConScore').value;
+    document.getElementById("ConMod").value= Math.floor((ConScore - 10)/2);
+
+    var DexScore = document.getElementById('DexScore').value;
+    document.getElementById("DexMod").value= Math.floor((DexScore - 10)/2);
+
+    var WisScore = document.getElementById('WisScore').value;
+    document.getElementById("WisMod").value= Math.floor((WisScore - 10)/2);
+
+    var CharScore = document.getElementById('CharScore').value;
+    document.getElementById("CharMod").value= Math.floor((CharScore - 10)/2);
+}
+
+function reflectProficiency() {
+    var Level = parseInt(document.getElementById('level').value);
+
+        if(Level >= 17) {
+            document.getElementById("proficiency").value = 6;
+        } else if (Level >= 13) {
+            document.getElementById("proficiency").value = 5;
+        } else if(Level >= 9) {
+            document.getElementById("proficiency").value = 4;
+        } else if (Level >= 5) {
+            document.getElementById("proficiency").value = 3;
+        } else {
+            document.getElementById("proficiency").value = 2;
+        }}
+
+function LevelUp() {
+    reflectProficiency();
+    reflectMod();
+}
+
+
+
+
+function reflectMod2() {
+    var StrScore2 = document.getElementById('StrScore2').value;
+    document.getElementById("StrMod2").value= Math.floor((StrScore2 - 10)/2);
+
+    var ConScore2 = document.getElementById('ConScore2').value;
+    document.getElementById("ConMod2").value= Math.floor((ConScore2 - 10)/2);
+
+    var DexScore2 = document.getElementById('DexScore2').value;
+    document.getElementById("DexMod2").value= Math.floor((DexScore2 - 10)/2);
+
+    var WisScore2 = document.getElementById('WisScore2').value;
+    document.getElementById("WisMod2").value= Math.floor((WisScore2 - 10)/2);
+
+    var CharScore2 = document.getElementById('CharScore2').value;
+    document.getElementById("CharMod2").value= Math.floor((CharScore2 - 10)/2);
+}
+
+
+function reflectProficiency2() {
+    var Level = parseInt(document.getElementById('level2').value);
+
+        if(Level >= 17) {
+            document.getElementById("proficiency2").value = 6;
+        } else if (Level >= 13) {
+            document.getElementById("proficiency2").value = 5;
+        } else if(Level >= 9) {
+            document.getElementById("proficiency2").value = 4;
+        } else if (Level >= 5) {
+            document.getElementById("proficiency2").value = 3;
+        } else {
+            document.getElementById("proficiency2").value = 2;
+        }}
+
+        function LevelUp2() {
+            reflectProficiency2();
+            reflectMod2();
+        }
