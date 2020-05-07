37f2300858452c60f22da33be439b44d47690bde
Jake <jake@Cynthias-MacBook-Pro.local>
2020-05-07 00:39:23 -0500

    project

--- app.js
+++ app.js
@@ -49,18 +49,38 @@ based on chosen skill (Var AttackModifier = hero.Attack - Enemy.AC
 
 
     for enemies: same but replace hero with enemy and enemy with hero.
-*/
-    var Modal = document.getElementById("Modal");
+// */
+//     var Modal = document.getElementById("Modal");
    
-    var start = document.getElementsByClassName("start");
+//     var start = document.getElementsByClassName("start");
 
-    onload = function () {
-        Modal.style.display = "block";
+//     onload = function () {
+//         Modal.style.display = "block";
+//     }
+function reflectHP1(){
+    var $HP1 = $('#ConMod').val()*$('#level').val()+100
+    $('.HP1').text(`${$HP1}`);
     }
 
-    
-
-
+    function Armor(activeArmor) {
+        var armor = activeArmor.value;
+        if (armor == 'cloth') {
+            document.getElementById('AC').value = parseInt(document.getElementById('IntMod').value) + 11;
+        } else if (armor == 'leather') {
+            document.getElementById('AC').value = parseInt(document.getElementById('DexMod').value) + 13;
+    } else if (armor == 'plate') {
+        document.getElementById('AC').value = parseInt(document.getElementById('StrMod').value) + 15;
+    } else {
+        document.getElementById('AC').value = parseInt(document.getElementById('ConMod').value) + 9;
+    }}
+// function reflectAC1(){
+//     var $AC1 = Armor().value
+//     $('.AC1').text(`AC: ${$AC1}`)
+// }
+function reflectHP2(){
+    var $HP2 = $('#ConMod2').val()*$('#level2').val()+100
+    $('.HP2').text(`HP: ${$HP2}`);
+}
     
 function reflectMod() {
     var StrScore = document.getElementById('StrScore').value;
@@ -77,6 +97,9 @@ function reflectMod() {
 
     var CharScore = document.getElementById('CharScore').value;
     document.getElementById("CharMod").value= Math.floor((CharScore - 10)/2);
+
+    reflectHP1();
+    // reflectAC1();
 }
 
 function reflectProficiency() {
@@ -97,6 +120,8 @@ function reflectProficiency() {
 function LevelUp() {
     reflectProficiency();
     reflectMod();
+    reflectHP1();
+    // reflectAC1();
 }
 
 
@@ -117,6 +142,7 @@ function reflectMod2() {
 
     var CharScore2 = document.getElementById('CharScore2').value;
     document.getElementById("CharMod2").value= Math.floor((CharScore2 - 10)/2);
+    reflectHP2();
 }
 
 
@@ -133,26 +159,18 @@ function reflectProficiency2() {
             document.getElementById("proficiency2").value = 3;
         } else {
             document.getElementById("proficiency2").value = 2;
+            
         }}
 
         function LevelUp2() {
             reflectProficiency2();
             reflectMod2();
+            reflectHP2();
         }
 
-        function Armor(activeArmor) {
-            var armor = activeArmor.value;
-            if (armor == 'cloth') {
-                document.getElementById('AC').value = parseInt(document.getElementById('IntMod').value) + 11;
-            } else if (armor == 'leather') {
-                document.getElementById('AC').value = parseInt(document.getElementById('DexMod').value) + 13;
-        } else if (armor == 'plate') {
-            document.getElementById('AC').value = parseInt(document.getElementById('StrMod').value) + 15;
-        } else {
-            document.getElementById('AC').value = parseInt(document.getElementById('ConMod').value) + 9;
-        }}
+       
 
-        function Armor2(activeArmor) {
+        function Armor2(activeArmor2) {
             var armor2 = activeArmor2.value;
             if (armor2 == 'cloth') {
                 document.getElementById('AC2').value = parseInt(document.getElementById('IntMod2').value) + 11;
@@ -163,3 +181,35 @@ function reflectProficiency2() {
         } else {
             document.getElementById('AC2').value = parseInt(document.getElementById('ConMod2').value) + 9;
         }}
+
+       
+
+
+
+function AttackP1() {
+HP = parseInt($('.HP2').val());
+NewHP= HP - 16;
+$('.HP2').text(`${NewHP}`);
+
+}
+function AttackP2() {
+    var TempHP = parseInt($('.HP1').val());
+    $('.HP1').text(`${NewHp}`);
+
+}
+
+
+
+
+
+
+
+
+
+
+        // $(Attack1 () {
+        //     $('Attack1').on('click', function () {
+                
+        //     })
+        // })
+    
